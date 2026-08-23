6a577bd770acbcf6c5d70e27a2e63260f28db606
Niaal-B <nihnihal504@gmail.com>
2026-08-23 19:43:58 +0530

    fix: image changed

--- src/components/CoupleSection.tsx
+++ src/components/CoupleSection.tsx
@@ -5,7 +5,7 @@ import { SectionHeading } from './Ornament';
 import { Butterflies } from './Butterflies';
 import { couple, type Person } from '../data/wedding';
 
-const COUPLE_PHOTO = '/couplepic.jpeg';
+const COUPLE_PHOTO = '/couplepic.png';
 
 function PersonCard({ person }: {person: Person;}) {
   return (
